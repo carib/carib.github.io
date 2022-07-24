3a02910d785fc996ecf6e7505104ffb3c6a07f57
Carib Guerra <carib.guerra@gmail.com>
2022-07-24 16:24:48 -0500

    Add button switch, countdown, fluid type, and btn_cloud

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-// NO OP
+const switchClass = () => {
+    const countdown = document.querySelector('.countdown_div');
+    const btn = document.querySelector('button');
+    const btnClassesQueue = ['cube', 'cloud'];
+    
+    countdown.classList.toggle('countdown');
+    
+    setTimeout(() => {
+        countdown.classList.toggle('countdown');
+        
+        btnClassesQueue.push(btnClassesQueue.shift());
+        btn.classList.toggle(`btn_${btnClassesQueue[0]}`);
+        
+        if (btn.classList.contains('btn')) {
+            btn.classList.remove('btn');
+        } else {
+            btn.classList.toggle(`btn_${btnClassesQueue[btnClassesQueue.length - 1]}`);
+        }
+    }, 5000);
+}
